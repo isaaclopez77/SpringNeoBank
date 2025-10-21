a52b1b9048bed1e31426e2a7b10acdb005c5c5ba
isaaclopez77 <isaaclopezzzz94@gmail.com>
2025-10-21 13:45:15 +0200

    Eureka microservice created, eureka client dependencies installed in parent pom

---
+++ infraestructure-domain/eureka/src/test/java/com/springneobank/eureka/EurekaApplicationTests.java
@@ -0,0 +1,13 @@
+package com.springneobank.eureka;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class EurekaApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
